1e0e64f11d2a9e71ff6b3cefe0ca0deaaa0cfa9c
decker <mdatmosci@gmail.com>
2024-11-05 09:48:49 -0600

    add file header about what to accomplish

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+//Mark Decker 11/5/2024
+//Rust-lang chapter 3 practice
+//generate the Nth Fibonacci number
+//
+//Must prompt user for N
+//print the Fibonacci sequence for N
+
+fn main() {
+    println!("Hello, world!");
+}

5904151968065c308dc57f7c896b02f39be80e36
decker <mdatmosci@gmail.com>
2024-11-05 10:21:04 -0600

    prompt user for input, loop unil we get a positive integer

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,31 @@
 //Must prompt user for N
 //print the Fibonacci sequence for N
 
+use std::io;
+
 fn main() {
+
+    let n_th: u32 = loop {
+
+        let mut n_th = String::new();
+
+        println!("Please input a positive integer:");
+
+        io::stdin()
+            .read_line(&mut n_th)
+            .expect("Failed to read line");
+
+        println!("You have enetered: {n_th}");
+ 
+        let n_th: u32 = match n_th
+            .trim()
+            .parse() {   //Handle the error
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+        break n_th
+    };
+
+
     println!("Hello, world!");
 }

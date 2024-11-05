6c3770671c7b1d4c09b2fc34d072f05ce39da046
decker <mdatmosci@gmail.com>
2024-11-05 10:36:51 -0600

    added recursive fibonacci function and output to screen

--- src/main.rs
+++ src/main.rs
@@ -21,15 +21,32 @@ fn main() {
 
         println!("You have enetered: {n_th}");
  
-        let n_th: u32 = match n_th
+        let n: u32 = match n_th
             .trim()
             .parse() {   //Handle the error
                 Ok(num) => num,
                 Err(_) => continue,
             };
-        break n_th
+        break n
     };
 
+    //could add all values to a vec to print on a single line
+    //print on multiple lines for simplicity
+    for i in 0..n_th {
+        println!("Fibonacci Sequence of {} is {}", i, gen_fib(i));
+    }
 
-    println!("Hello, world!");
 }
+
+//define a recursive fibonacci funtion that returns next number
+fn gen_fib (n_th: u32) -> u32 {
+    if n_th <= 0 {
+        return 0;
+    } else if n_th == 1 {
+        return 1;
+    } else {
+        return gen_fib(n_th-1)  + gen_fib(n_th-2);
+    }
+}
+
+
